15b81313940cdf6a1fed125fa00093b8e9af8b7d
NAVINASH26 <avinash.n@atenas.in>
2026-02-23 21:39:12 +0530

    minor change

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -20,7 +20,7 @@ def get_db():
 
 
 def get_current_user(
-    token: str = Depends(oauth2_scheme),   # 👈 Correct usage
+    token: str = Depends(oauth2_scheme),   
     db: Session = Depends(get_db)
 ):
     try:
